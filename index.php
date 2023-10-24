76ffe5086a5da9591d13d8dd29eccc5ba9da4dda
Kim Urdahl-Aasen <cjmozart@gmail.com>
2023-10-24 17:36:48 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+//landingsside for Bare Vifter - selskapet som utelukkende selger vifter
+
+//Sjekk om logget inn 
+
+
+//HTML
+?>
+<!-- -------->
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+<body>
+
+
+
+
+
+
+
+</body>
+</html>
+<!-- -------->
+<?php
+
+?>
